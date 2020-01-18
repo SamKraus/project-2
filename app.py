29e86c350f67a9b2d2b299d075f9540954f9c4d8
Emayer <53119577+jeneric1023@users.noreply.github.com>
2020-01-18 00:43:54 -0600

    app.py working w/ database

--- app.py
+++ app.py
@@ -1,15 +1,11 @@
 # Import the functions we need from flask
-#import psycopg2
-#import sys
-import numpy as numpy
+import numpy as np
 import pandas as pd 
 import sqlalchemy
 from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
 from sqlalchemy import create_engine, func
 from flask import Flask, jsonify, render_template
-
-
 
 #############################################
 # Datatbase Setup
@@ -30,22 +26,8 @@ Base.prepare(engine, reflect=True)
 # Save reference to the table
 Superbowls = Base.classes.superbowl
 
-data = pd.read_sql('select * from superbowl', connection)
-session = Session(engine)
 
-results = []
-
-for s in session.query(Superbowls).all():
-    results.append({'sb': s.sb, 'attendance': s.attendance, 'winner': s.winner,
-    'winning pts': s.winning_pts,
-    'loser': s.loser, 'losing pts': s.losing_pts, 'mvp': s.mvp, 'stadium': s.stadium, 'city, state': s.city_state,
-    'lat': s.lat, 'long': s.lng})
-
-print(results)
-
-session.close()
-
-##########################################
+##############################################
 # Flask Setup
 #########################################
 
@@ -53,80 +35,128 @@ session.close()
 # Think of it as chocolate cake recipe. 
 app = Flask(__name__)
 
+#########################################
+
 @app.route("/")
 def welcome():
     """list all routes"""
-    return render_template("index.html", sb=results[5])
-    # return(
-    #     f"Available Routes:<br/>"
-    #     f"/api/v1.0/home<br/>"
-    #     f"/api/v1.0/superbowls<br/>"
-    #     f"/api/v1.0/dict<br/>"
-    #     f"/api/v1.0/test<br/>"
-  f"/api/v1.0/jsonified"
-    # )
+   
+    return(
+        f"Available Routes:<br/>"
+        f"/api/v1.0/data<br/>"
+        f"/api/v1.0/map<br/>"
+        f"/api/v1.0/other<br/>"
+        f"/api/v1.0/jsonified<br/>"
+        f"/api/v1.0/"
+ )
+
+###################################################
+
+# Here's where we define the various application routes ...
+@app.route("/")
+def IndexRoute():
+    ''' This function runs when the browser loads the index route. 
+        Note that the html file must be located in a folder called templates. '''
+
+    webpage = render_template("index.html")
+    return webpage
+#################################################
+
+@app.route("/other")
+def OtherRoute():
+    ''' This function runs when the user clicks the link for the other page.
+        Note that the html file must be located in a folder called templates. '''
+
+    # Note that this call to render template passes in the title parameter. 
+    # That title parameter is a 'Shirley' variable that could be called anything 
+    # we want. But, since we're using it to specify the page title, we call it 
+    # what we do. The name has to match the parameter used in other.html. 
+    webpage = render_template("other.html", title_we_want="Shirley")
+    return webpage
+
+# ###########################################################
+@app.route("/data")
+def DataRoute():
+    session = Session(engine)
+    # Query all passengers
+    results = session.query(Superbowls.sb).all()
+    session.close()
+    # Convert list of tuples into normal list
+    all_names = list(np.ravel(results))
+    return jsonify(all_names)
+
+    ''' This function runs when the user clicks the link for the other page.
+        Note that the html file must be located in a folder called templates. '''
+
+    # Note that this call to render template passes in the title parameter. 
+    # That title parameter is a 'Shirley' variable that could be called anything 
+    # we want. But, since we're using it to specify the page title, we call it 
+    # what we do. The name has to match the parameter used in other.html. 
+   
+#################################################################
 
 
-# # Here's where we define the various application routes ...
-# @app.route("/")
-# def IndexRoute():
-#     ''' This function runs when the browser loads the index route. 
-#         Note that the html file must be located in a folder called templates. '''
+@app.route("/map")
+def mapRoute():
 
-#     webpage = render_template("index.html")
-#     return webpage
+    session = Session(engine)
 
-# @app.route("/other")
-# def OtherRoute():
-#     ''' This function runs when the user clicks the link for the other page.
-#         Note that the html file must be located in a folder called templates. '''
+    map_results = session.query(Superbowls.sb, Superbowls.city_state,
+    Superbowls.lat, Superbowls.lng).all()
 
-#     # Note that this call to render template passes in the title parameter. 
-#     # That title parameter is a 'Shirley' variable that could be called anything 
-#     # we want. But, since we're using it to specify the page title, we call it 
-#     # what we do. The name has to match the parameter used in other.html. 
-#     webpage = render_template("other.html", title_we_want="Shirley")
-#     return webpage
+    session.close()
 
-@app.route("/test")
-def TestRoute():
+# Create dictionary of map coordinates
+
+    coordinates = []
+
+    for sb, city_state, lat, lng in map_results:
+        coordinates_dict = {}
+        coordinates_dict["superbowl"] = sb
+        coordinates_dict["city_state"] = city_state
+        coordinates_dict["lat"] = lat
+        coordinates_dict["long"] = lng
+
+        coordinates.append(coordinates_dict)
+
+
+    return jsonify(coordinates)
+
+
     ''' This function returns a simple message, just to guarantee that
         the Flask server is working. '''
 
-    return "This is the test route!"
 
-# @app.route("/dictionary")
-# def DictionaryRoute():
-#     ''' This function returns a jsonified dictionary. Ideally we'd create 
-#         that dictionary from a database query. '''
-
-#     dict = { "Tequila": 10,
-#              "Beer": 2,
-#              "Red Wine": 8,
-#              "White Wine": 1}
-    
-#     return jsonify(dict) # Return the jsonified version of the dictionary
-
+###################################################
+#   Return the jsonified version of the dictionary
+###################################################
 
 @app.route("/jsonified")
+
 def jsonified():
+
+    data = pd.read_sql('select * from superbowl', connection)
+    
+    session = Session(engine)
+
+    results = []
+
+    for s in session.query(Superbowls).all():
+        results.append({'sb': s.sb, 'attendance': s.attendance, 
+        'winner': s.winner, 'winning pts': s.winning_pts,'loser': s.loser,
+        'losing pts': s.losing_pts, 'mvp': s.mvp, 'stadium': s.stadium,
+        'city, state': s.city_state, 'lat': s.lat, 'long': s.lng})
+
+    print(results)
+
+    session.close()
+
     return jsonify(results)
 
 
-# @app.route("/dict")
-# def DictRoute():
-#     ''' This seems to work in the latest versions of Chrome. But it's WRONG to
-#         return a dictionary (or any Python-specific datatype) without jsonifying
-#         it first! '''        
-
-#     dict = { "one": 1,
-#              "two": 2,
-#              "three": 3}
-    
-#     return dict # Don't return a dictionary! 
-
-
-# # This statement is required for Flask to do its job. 
-# # Think of it as chocolate cake recipe. 
-# if __name__ == '__main__':
-#     app.run(debug=True)
+#########################################################
+# This statement is required for Flask to do its job. 
+# Think of it as chocolate cake recipe. 
+#########################################################
+if __name__ == '__main__':
+    app.run(debug=True)
--- static/main.js
+++ static/main.js
@@ -2,33 +2,67 @@
 console.log("Loaded main.js");
 
 // Query the endpoint that returns a JSON ...
-d3.json("/dictionary").then(function (data) {
+d3.json("/map").then(function (data) {
 
     // ... and dump that JSON to the console for inspection
     console.log(data); 
+    console.log("lat"+data.lat)
+ 
+    function createMap(superBowls) {
 
-    // Next, pull out the keys and the values for graphing
-    keys = Object.keys(data);
-    values = Object.values(data);
+        // Create the tile layer that will be the background of our map
+        var lightmap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/light-v9/tiles/256/{z}/{x}/{y}?access_token={accessToken}", {
+            attribution: "Map data &copy; <a href=\"http://openstreetmap.org\">OpenStreetMap</a> contributors, <a href=\"http://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"http://mapbox.com\">Mapbox</a>",
+            maxZoom: 18,
+            id: "mapbox.light",
+            accessToken: API_KEY
+        });
 
-    // Create the trace
-    var trace = {
-        x: keys,
-        y: values,
-        type: "bar"
-    };
+        // Create a baseMaps object to hold the lightmap layer
+        var baseMaps = {
+            "Light Map": lightmap
+        };
 
-    // Put the trace into an array (which allows us to graph
-    // multiple traces, if we wish)
-    var data = [trace];
+        // Create an overlayMaps object to hold the bikeStations layer
+        var overlayMaps = {
+            "Super Bowls": superBowls
+        };
 
-    // Define a layout object
-    var layout = {
-        title: "'Bar' Chart",
-        xaxis: { title: "Drinks"},
-        yaxis: { title: "Rating"}
-    };
+        // Create the map object with options
+        var map = L.map("plot", {
+            center: [39.828175,-98.579500],
+            zoom: 5,
+            layers: [lightmap, superBowls]
+        });
 
-    // Create the plot
-    Plotly.newPlot("plot", data, layout); 
+        // Create a layer control, pass in the baseMaps and overlayMaps. Add the layer control to the map
+        L.control.layers(baseMaps, overlayMaps, {
+            collapsed: false
+        }).addTo(map);
+    }
+    
+    function createMarkers(response) {
+    
+        // Pull the "stations" property off of response.data
+        var locations = response.city_state;
+    
+        // Initialize an array to hold bike markers
+        var cityMarkers = [];
+    
+        // Loop through the stations array
+        for (var index = 0; index < locations.length; index++) {
+        var locationList = locations[index];
+    
+        // For each station, create a marker and bind a popup with the station's name
+        var cityMarker = L.marker([location.lat, location.long])
+            .bindPopup("<h3>Super Bowl" + location.superbowl + "<h3>");
+    
+        // Add the marker to the bikeMarkers array
+        cityMarkers.push(cityMarker);
+        }
+    
+        // Create a layer group made from the bike markers array, pass it into the createMap function
+        createMap(L.layerGroup(cityMarkers));
+    }
+  
 });
